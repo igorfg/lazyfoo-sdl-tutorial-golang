3bb01df7700a16f04c5b0a0b567d9c937a568fe6
igorfg <igorfgds@gmail.com>
2018-06-24 16:50:30 -0300

    finished lesson 12

---
+++ 12_color_modulation/LTexture.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/veandco/go-sdl2/img"
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+//LTexture is a sdl.Texture wrapper class
+type LTexture struct {
+	//The actual hardware texture
+	mTexture *sdl.Texture
+
+	//image dimensions
+	mWidth  int32
+	mHeight int32
+}
+
+//NewLTexture initializes variables
+func NewLTexture() *LTexture {
+	//Initialize
+	return &LTexture{mTexture: nil, mWidth: 0, mHeight: 0}
+}
+
+//LoadFromFile loads image at specified path
+func (lt *LTexture) LoadFromFile(path string) error {
+	//Get rid of preexisting texture
+	err := lt.Free()
+	if err != nil {
+		return fmt.Errorf("could not free LTexture: %v", err)
+	}
+
+	//The final texture
+	var newTexture *sdl.Texture
+
+	//Load image at specified path
+	loadedSurface, err := img.Load(path)
+	if err != nil {
+		return fmt.Errorf("could not load image %v! SDL_image Error: %v", path, err)
+	}
+
+	//Color key image
+	err = loadedSurface.SetColorKey(true, sdl.MapRGB(loadedSurface.Format, 0, 255, 255))
+	if err != nil {
+		return fmt.Errorf("could not set color key: %v", err)
+	}
+
+	//Create texture from surface pixels
+	newTexture, err = gRenderer.CreateTextureFromSurface(loadedSurface)
+	if err != nil {
+		return fmt.Errorf("could not create texture from %v pixels: %v", path, err)
+	}
+
+	//Get image dimensions
+	lt.mWidth = loadedSurface.W
+	lt.mHeight = loadedSurface.H
+
+	//Get rid of old loaded surface
+	loadedSurface.Free()
+
+	//Assing new texture
+	lt.mTexture = newTexture
+
+	return nil
+}
+
+//Free deallocates memory
+func (lt *LTexture) Free() error {
+	//Free texture if it exists
+	if lt.mTexture != nil {
+		err := lt.mTexture.Destroy()
+		if err != nil {
+			return fmt.Errorf("could not destroy LTexture: %v", err)
+		}
+
+		lt.mTexture = nil
+		lt.mWidth = 0
+		lt.mHeight = 0
+	}
+	return nil
+}
+
+//SetColor sets color modulation
+func (lt *LTexture) SetColor(red, green, blue uint8) error {
+	//Modulate texture
+	err := lt.mTexture.SetColorMod(red, green, blue)
+	if err != nil {
+		return fmt.Errorf("could not set color mod for texture: %v", err)
+	}
+	return nil
+}
+
+//Render renders texture at given point
+func (lt *LTexture) Render(x, y int32, clip *sdl.Rect) error {
+	//Set rendering space and render to screen
+	renderQuad := sdl.Rect{X: x, Y: y, W: lt.mWidth, H: lt.mHeight}
+
+	if clip != nil {
+		renderQuad.W = clip.W
+		renderQuad.H = clip.H
+	}
+
+	err := gRenderer.Copy(lt.mTexture, clip, &renderQuad)
+	if err != nil {
+		return fmt.Errorf("could not copy texture: %v", err)
+	}
+
+	return nil
+}
+
+//GetWidth gets image width
+func (lt *LTexture) GetWidth() int32 {
+	return lt.mWidth
+}
+
+//GetHeight gets image height
+func (lt *LTexture) GetHeight() int32 {
+	return lt.mHeight
+}
---
+++ 12_color_modulation/main.go
@@ -0,0 +1,213 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/veandco/go-sdl2/img"
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+const (
+	//Screen dimension constants
+	screenWitdh  = 640
+	screenHeight = 480
+)
+
+var (
+	//The window we'll be rendering to
+	gWindow *sdl.Window
+
+	//The window renderer
+	gRenderer *sdl.Renderer
+
+	//Scene texture
+	gModulatedTexture LTexture
+)
+
+func main() {
+	//Start up SDL and create window
+	if err := initSDl(); err != nil {
+		log.Fatalf("Could not init SDL: %v\n", err)
+	}
+
+	//Load media
+	if err := loadMedia(); err != nil {
+		log.Fatalf("Could not load media: %v\n", err)
+	}
+
+	//Main loop flag
+	quit := false
+
+	//Event handler
+	var e sdl.Event
+
+	//Modulation components
+	var r uint8 = 255
+	var g uint8 = 255
+	var b uint8 = 255
+
+	//While application is running
+	for !quit {
+		//Handle events on queue
+		for e = sdl.PollEvent(); e != nil; e = nil {
+			//User requests quit
+			if e.GetType() == sdl.QUIT {
+				quit = true
+			} else if e.GetType() == sdl.KEYDOWN { //On keypress change rgb values
+				switch e.(*sdl.KeyboardEvent).Keysym.Sym {
+				//Increase red
+				case sdl.K_q:
+					r += 32
+					break
+
+				//Increase green
+				case sdl.K_w:
+					g += 32
+					break
+
+				//Increase blue
+				case sdl.K_e:
+					b += 32
+					break
+
+				//Decrease red
+				case sdl.K_a:
+					r -= 32
+					break
+
+				//Decrease green
+				case sdl.K_s:
+					g -= 32
+					break
+
+				//Decrease blue
+				case sdl.K_d:
+					b -= 32
+					break
+				}
+			}
+		}
+
+		//Clear screen
+		err := gRenderer.SetDrawColor(255, 255, 255, 255)
+		if err != nil {
+			log.Fatalf("could not set draw color for renderer: %v", err)
+		}
+
+		err = gRenderer.Clear()
+		if err != nil {
+			log.Fatalf("could not clear renderer: %v", err)
+		}
+
+		//Modulate and render texture
+		err = gModulatedTexture.SetColor(r, g, b)
+		if err != nil {
+			log.Fatalf("could not set color from texture: %v", err)
+		}
+		err = gModulatedTexture.Render(0, 0, nil)
+		if err != nil {
+			log.Fatalf("could not render modulated texture: %v", err)
+		}
+
+		//Update screen
+		gRenderer.Present()
+	}
+
+	//Free resources and close SDL
+	if err := close(); err != nil {
+		log.Fatalf("Could not close SDL! SDL Error: %v\n", err)
+	}
+}
+
+func initSDl() error {
+	//Local error declaration
+	var err error
+
+	//Initialize SDL
+	if err := sdl.Init(sdl.INIT_VIDEO); err != nil {
+		return fmt.Errorf("SDL could not initialize! SDL_ERROR: %v", err)
+	}
+
+	//Set texture filtering to linear
+	if !sdl.SetHint(sdl.HINT_RENDER_SCALE_QUALITY, "1") {
+		fmt.Printf("Warning: Linear texture filtering not enabled!")
+	}
+
+	//Create Window
+	gWindow, err = sdl.CreateWindow("SDL Tutorial", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
+		screenWitdh, screenHeight, sdl.WINDOW_SHOWN)
+	if err != nil {
+		return fmt.Errorf("Window could not be created! SDL_Error: %v", err)
+	}
+
+	//Create renderer for window
+	if gRenderer, err = sdl.CreateRenderer(gWindow, -1, sdl.RENDERER_ACCELERATED); err != nil {
+		return fmt.Errorf("Renderer could not be created! SDL Error: %v", err)
+	}
+
+	//Initialize renderer color
+	gRenderer.SetDrawColor(255, 255, 255, 255)
+
+	//Initialize PNG loading
+	imgFlags := img.INIT_PNG
+	if (img.Init(imgFlags) & imgFlags) == 0 {
+		return fmt.Errorf("SDL_image could not initialize! SDL_image Error: %v", img.GetError())
+	}
+
+	return nil
+}
+
+func loadMedia() error {
+	//Load Foo' texture
+	err := gModulatedTexture.LoadFromFile("colors.png")
+	if err != nil {
+		return fmt.Errorf("failed to load sprite sheet texture: %v", err)
+	}
+
+	return nil
+}
+
+func close() error {
+	//Free loaded images
+	if err := gModulatedTexture.Free(); err != nil {
+		return fmt.Errorf("could not free modulated texture: %v", err)
+	}
+
+	//Destroy window
+	if err := gRenderer.Destroy(); err != nil {
+		return fmt.Errorf("Could not destroy global renderer: %v", err)
+	}
+	if err := gWindow.Destroy(); err != nil {
+		return fmt.Errorf("Could not destroy window: %v", err)
+	}
+	gWindow = nil
+	gRenderer = nil
+
+	//Quit SDL Subsystems
+	img.Quit()
+	sdl.Quit()
+	return nil
+}
+
+func loadTexture(path string) (*sdl.Texture, error) {
+	//The final texture
+	var newTexture *sdl.Texture
+
+	//Load image at specified path
+	loadedSurface, err := img.Load(path)
+	if err != nil {
+		return nil, fmt.Errorf("Unable to load image %v! SDL_image: %v", path, err)
+	}
+
+	//Create texture from surface pixels
+	newTexture, err = gRenderer.CreateTextureFromSurface(loadedSurface)
+	if err != nil {
+		return nil, fmt.Errorf("Unable to create texture from %v! SDL Error: %v", path, err)
+	}
+
+	//Get rid of old loaded surface
+	loadedSurface.Free()
+
+	return newTexture, nil
+}
