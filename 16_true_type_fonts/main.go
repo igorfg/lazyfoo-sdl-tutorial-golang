629cfa52af98c8581c2f7c545e27686528f6ce01
igorfg <igorfgds@gmail.com>
2018-06-24 21:51:24 -0300

    finished lesson 16

---
+++ 16_true_type_fonts/main.go
@@ -0,0 +1,200 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/veandco/go-sdl2/img"
+	"github.com/veandco/go-sdl2/sdl"
+	"github.com/veandco/go-sdl2/ttf"
+)
+
+//Screen dimension constants
+const (
+	screenWitdh  = 640
+	screenHeight = 480
+)
+
+var (
+	//The window we'll be rendering to
+	gWindow *sdl.Window
+
+	//The window renderer
+	gRenderer *sdl.Renderer
+
+	//Globally used font
+	gFont *ttf.Font
+
+	//Rendered texture
+	gTextTexture LTexture
+)
+
+func main() {
+	//Start up SDL and create window
+	if err := initSDl(); err != nil {
+		log.Fatalf("Could not init SDL: %v\n", err)
+	}
+
+	//Load media
+	if err := loadMedia(); err != nil {
+		log.Fatalf("Could not load media: %v\n", err)
+	}
+
+	//Main loop flag
+	quit := false
+
+	//Event handler
+	var e sdl.Event
+
+	//Angle of rotation
+	var degrees float64
+
+	//Flip type
+	var flipType = sdl.FLIP_NONE
+
+	//While application is running
+	for !quit {
+		//Handle events on queue
+		for e = sdl.PollEvent(); e != nil; e = nil {
+			//User requests quit
+			if e.GetType() == sdl.QUIT {
+				quit = true
+			}
+		}
+
+		//Clear screen
+		err := gRenderer.SetDrawColor(255, 255, 255, 255)
+		if err != nil {
+			log.Fatalf("could not set draw color for renderer: %v", err)
+		}
+		err = gRenderer.Clear()
+		if err != nil {
+			log.Fatalf("could not clear renderer: %v", err)
+		}
+
+		//Render current frame
+		err = gTextTexture.Render((screenWitdh-gTextTexture.GetWidth())/2,
+			(screenHeight-gTextTexture.GetHeight())/2, nil, degrees, nil, flipType)
+
+		//Update screen
+		gRenderer.Present()
+	}
+
+	//Free resources and close SDL
+	if err := close(); err != nil {
+		log.Fatalf("Could not close SDL! SDL Error: %v\n", err)
+	}
+}
+
+func initSDl() error {
+	//Local error declaration
+	var err error
+
+	//Initialize SDL
+	if err := sdl.Init(sdl.INIT_VIDEO); err != nil {
+		return fmt.Errorf("SDL could not initialize! SDL_ERROR: %v", err)
+	}
+
+	//Set texture filtering to linear
+	if !sdl.SetHint(sdl.HINT_RENDER_SCALE_QUALITY, "1") {
+		fmt.Printf("Warning: Linear texture filtering not enabled!")
+	}
+
+	//Create Window
+	gWindow, err = sdl.CreateWindow("SDL Tutorial", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
+		screenWitdh, screenHeight, sdl.WINDOW_SHOWN)
+	if err != nil {
+		return fmt.Errorf("Window could not be created! SDL_Error: %v", err)
+	}
+
+	//Create vsynced renderer for window
+	if gRenderer, err = sdl.CreateRenderer(gWindow, -1, sdl.RENDERER_ACCELERATED|sdl.RENDERER_PRESENTVSYNC); err != nil {
+		return fmt.Errorf("Renderer could not be created! SDL Error: %v", err)
+	}
+
+	//Initialize renderer color
+	gRenderer.SetDrawColor(255, 255, 255, 255)
+
+	//Initialize PNG loading
+	imgFlags := img.INIT_PNG
+	if (img.Init(imgFlags) & imgFlags) == 0 {
+		return fmt.Errorf("SDL_image could not initialize! SDL_image Error: %v", img.GetError())
+	}
+
+	//Initialize SDL_ttf
+	if err := ttf.Init(); err != nil {
+		return fmt.Errorf("SDL_ttf coul not initialize! SDL_ttf Error: %v", err)
+	}
+
+	return nil
+}
+
+func loadMedia() error {
+	//Local error declaration
+	var err error
+
+	//Open the font
+	gFont, err = ttf.OpenFont("lazy.ttf", 28)
+	if err != nil {
+		return fmt.Errorf("failed to load lazy font! SDL_ttf Error: %v", err)
+	}
+
+	//Render text
+	textColor := sdl.Color{R: 0, G: 0, B: 0, A: 0}
+	err = gTextTexture.loadFromRenderedText("The quick brown fox jumps over the lazy dog", textColor)
+	if err != nil {
+		return fmt.Errorf("failed to render text texture: %v", err)
+	}
+
+	return nil
+}
+
+func close() error {
+	//Free loaded images
+	if err := gTextTexture.Free(); err != nil {
+		return fmt.Errorf("could not free text texture: %v", err)
+	}
+
+	//Free global font
+	gFont.Close()
+	gFont = nil
+
+	//Destroy window
+	if err := gRenderer.Destroy(); err != nil {
+		return fmt.Errorf("Could not destroy global renderer: %v", err)
+	}
+	if err := gWindow.Destroy(); err != nil {
+		return fmt.Errorf("Could not destroy window: %v", err)
+	}
+	gWindow = nil
+	gRenderer = nil
+
+	//Quit SDL Subsystems
+	ttf.Quit()
+	img.Quit()
+	sdl.Quit()
+
+	return nil
+}
+
+func loadTexture(path string) (*sdl.Texture, error) {
+	//The final texture
+	var newTexture *sdl.Texture
+
+	//Load image at specified path
+	loadedSurface, err := img.Load(path)
+	if err != nil {
+		return nil, fmt.Errorf("Unable to load image %v! SDL_image: %v", path, err)
+	}
+
+	//Create texture from surface pixels
+	newTexture, err = gRenderer.CreateTextureFromSurface(loadedSurface)
+	if err != nil {
+		return nil, fmt.Errorf("Unable to create texture from %v! SDL Error: %v", path, err)
+	}
+
+	//Get rid of old loaded surface
+	loadedSurface.Free()
+
+	return newTexture, nil
+}
